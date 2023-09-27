d272eb5645a974601638f414c77e078d7408df7d
Koniczyn <piotr12konczyk27@gmail.com>
2023-09-27 22:14:44 +0200

    Changed cookies alert dialog

--- src/components/cookies/Cookies.tsx
+++ src/components/cookies/Cookies.tsx
@@ -28,27 +28,22 @@ const Cookies: FC<CookiesProps> = () => {
     return (
         mounted && (
             <AlertDialog defaultOpen>
-                <AlertDialogContent>
+                <AlertDialogContent className="border-slate-700">
                     <AlertDialogHeader>
                         <AlertDialogTitle>Cookies Policy</AlertDialogTitle>
                     </AlertDialogHeader>
-                    <AlertDialogDescription className="text-gray-400 text-lg mt-4">
-                        Hello! We use cookies and localStorage to enhance your
-                        experience on our website. They help us analyze traffic,
-                        personalize content, and improve functionality. By using
-                        our site, you consent to cookie usage. Manage settings
-                        in your browser anytime. Learn more in our terms and
-                        conditions.
+                    <AlertDialogDescription className="text-sec text-lg mt-4">
+                        Hello! I&apos;m using cookies and localStorage to
+                        enhance your experience on my website. They help me save
+                        the theme, improve functionality and overall experience.
+                        By using my site, you agree to cookie usage. Manage
+                        settings in your browser anytime. Learn more in our
+                        terms and conditions.
                     </AlertDialogDescription>
 
                     <AlertDialogFooter>
                         <AlertDialogTrigger asChild>
-                            <Button
-                                className="bg-slate-200 hover:bg-white text-black"
-                                onClick={handleAccept}
-                            >
-                                Accept
-                            </Button>
+                            <Button onClick={handleAccept}>Accept</Button>
                         </AlertDialogTrigger>
                     </AlertDialogFooter>
                 </AlertDialogContent>
