175ce025fe14528891fdfd2e32ac7772aa8cd5c3
Koniczyn <piotr12konczyk27@gmail.com>
2023-10-01 13:00:18 +0200

    Deployment with postinstall

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,6 @@ module.exports = {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: ['@typescript-eslint', 'react'],
+    plugins: ['@typescript-eslint', 'react', 'next'],
     rules: {},
 };
