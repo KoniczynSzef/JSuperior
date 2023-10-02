084ecb5b9acc04ee62a7cabe46d4261cbae6253e
Koniczyn <piotr12konczyk27@gmail.com>
2023-10-02 22:30:12 +0200

    Trying to repair fetch for deployment

--- src/app/lessons/[id]/page.tsx
+++ src/app/lessons/[id]/page.tsx
@@ -20,14 +20,12 @@ export const fetchLesson = async (id: number) => {
     const res = await fetch(`${process.env.BASE_NEXT_URL}/api/lessons/${id}`, {
         body: JSON.stringify({ id }),
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
     });
 
     if (res.status !== 200) throw new Error('Error while fetching');
 
     const data: Lesson = await res.json();
+
     return data;
 };
 
@@ -35,9 +33,6 @@ const fetchQuiz = async (id: number) => {
     const res = await fetch(`${process.env.BASE_NEXT_URL}/api/quiz/${id}`, {
         method: 'POST',
         body: JSON.stringify({ id }),
-        headers: {
-            'Content-Type': 'application/json',
-        },
     });
 
     if (res.status !== 200) throw new Error('Error while fetching');
@@ -47,54 +42,54 @@ const fetchQuiz = async (id: number) => {
     return data;
 };
 
-const page: FC<pageProps> = async ({ params }) => {
+const page: FC<pageProps> = async ({ params = { id: '1' } }) => {
     const session = await getServerSession(authOptions);
     if (!session?.user) return redirect('/signin');
 
-    const lesson = await fetchLesson(parseInt(params.id));
-    const prevLesson = await fetchLesson(parseInt(params.id) - 1);
-    const nextLesson = await fetchLesson(parseInt(params.id) + 1);
+    // const lesson = await fetchLesson(parseInt(params.id));
+    // const prevLesson = await fetchLesson(parseInt(params.id) - 1);
+    // const nextLesson = await fetchLesson(parseInt(params.id) + 1);
 
-    const quiz = await fetchQuiz(parseInt(params.id));
+    // const quiz = await fetchQuiz(parseInt(params.id));
 
     return (
-        <div className="relative my-16 text-left w-full">
-            <h2 className="text-4xl font-semibold">{lesson.title}</h2>
-            <p className="mt-5">{lesson.description}</p>
+        <h2>Hello World!</h2>
+        // <div className="relative my-16 text-left w-full">
+        //     <h2 className="text-4xl font-semibold">{lesson.title}</h2>
+        //     <p className="mt-5">{lesson.description}</p>
 
-            <Markdown content={lesson.content} />
+        //     <Markdown content={lesson.content} />
 
-            {quiz && <QuizWrapper quiz={quiz} />}
+        //     {quiz && <QuizWrapper quiz={quiz} />}
 
-            <div className="flex mt-8 justify-between">
-                {prevLesson !== null && (
-                    <Link
-                        href={`/lessons/${
-                            prevLesson.id === 1 ? '' : prevLesson.id
-                        }`}
-                        className="group"
-                    >
-                        <span className="flex items-center gap-3 text-sec group-hover:text-accent-foreground transition">
-                            <ArrowLeft />
-                            {prevLesson.title}{' '}
-                        </span>
-                    </Link>
-                )}
+        //     <div className="flex mt-8 justify-between">
+        //         {prevLesson.title.length >= 1 && (
+        //             <Link
+        //                 href={`/lessons/${
+        //                     prevLesson.id === 1 ? '' : prevLesson.id
+        //                 }`}
+        //                 className="group"
+        //             >
+        //                 <span className="flex items-center gap-3 text-sec group-hover:text-accent-foreground transition">
+        //                     <ArrowLeft />
+        //                     {prevLesson.title}
+        //                 </span>
+        //             </Link>
+        //         )}
 
-                {nextLesson !== null && (
-                    <Link
-                        href={`/lessons/${nextLesson.id}`}
-                        className="group ml-auto"
-                    >
-                        <span className="flex items-center gap-3 text-sec group-hover:text-accent-foreground transition">
-                            {nextLesson.title}
-
-                            <ArrowRight />
-                        </span>
-                    </Link>
-                )}
-            </div>
-        </div>
+        //         {nextLesson.title.length >= 1 && (
+        //             <Link
+        //                 href={`/lessons/${nextLesson.id}`}
+        //                 className="group ml-auto"
+        //             >
+        //                 <span className="flex items-center gap-3 text-sec group-hover:text-accent-foreground transition">
+        //                     {nextLesson.title}
+        //                     <ArrowRight />
+        //                 </span>
+        //             </Link>
+        //         )}
+        //     </div>
+        // </div>
     );
 };
 
