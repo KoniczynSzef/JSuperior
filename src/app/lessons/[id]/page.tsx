2fa33ed29483b7027bd181119f3e82e7bcae1d44
Koniczyn <piotr12konczyk27@gmail.com>
2023-10-02 22:36:20 +0200

    Trying to repair fetch for deployment v2

--- src/app/lessons/[id]/page.tsx
+++ src/app/lessons/[id]/page.tsx
@@ -2,11 +2,12 @@ import { authOptions } from '@/utils/authOptions';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import React, { FC } from 'react';
-import Markdown from './Markdown';
-import { Lesson, Quiz as QuizType } from '@prisma/client';
-import QuizWrapper from './QuizWrapper';
-import Link from 'next/link';
-import { ArrowLeft, ArrowRight } from 'lucide-react';
+// import Markdown from './Markdown';
+import { Lesson } from '@prisma/client';
+// import { Lesson, Quiz as QuizType } from '@prisma/client';
+// import QuizWrapper from './QuizWrapper';
+// import Link from 'next/link';
+// import { ArrowLeft, ArrowRight } from 'lucide-react';
 
 interface pageProps {
     params: {
@@ -29,22 +30,23 @@ export const fetchLesson = async (id: number) => {
     return data;
 };
 
-const fetchQuiz = async (id: number) => {
-    const res = await fetch(`${process.env.BASE_NEXT_URL}/api/quiz/${id}`, {
-        method: 'POST',
-        body: JSON.stringify({ id }),
-    });
+// const fetchQuiz = async (id: number) => {
+//     const res = await fetch(`${process.env.BASE_NEXT_URL}/api/quiz/${id}`, {
+//         method: 'POST',
+//         body: JSON.stringify({ id }),
+//     });
 
-    if (res.status !== 200) throw new Error('Error while fetching');
+//     if (res.status !== 200) throw new Error('Error while fetching');
 
-    const data: QuizType = await res.json();
+//     const data: QuizType = await res.json();
 
-    return data;
-};
+//     return data;
+// };
 
 const page: FC<pageProps> = async ({ params = { id: '1' } }) => {
     const session = await getServerSession(authOptions);
     if (!session?.user) return redirect('/signin');
+    params;
 
     // const lesson = await fetchLesson(parseInt(params.id));
     // const prevLesson = await fetchLesson(parseInt(params.id) - 1);
