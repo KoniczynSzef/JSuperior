9013407f4e82129b5dcd22f7a12602d003b1c0ab
Koniczyn <piotr12konczyk27@gmail.com>
2023-10-03 22:19:15 +0200

    Commit

--- src/app/lessons/page.tsx
+++ src/app/lessons/page.tsx
@@ -1,25 +1,24 @@
-// import { authOptions } from '@/utils/authOptions';
-// import { getServerSession } from 'next-auth';
-// import Link from 'next/link';
-// import { redirect } from 'next/navigation';
+import { authOptions } from '@/utils/authOptions';
+import { getServerSession } from 'next-auth';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 import React, { FC } from 'react';
-// import { ArrowRight } from 'lucide-react';
+import { ArrowRight } from 'lucide-react';
 
-// import ReactMarkdown from 'react-markdown';
-// import { fetchLesson } from './[id]/page';
+import ReactMarkdown from 'react-markdown';
+import { fetchLesson } from '@/utils/fetchFunctions';
 
 interface pageProps {}
 
 const page: FC<pageProps> = async () => {
-    // const session = await getServerSession(authOptions);
-    // if (!session?.user) return redirect('/signin');
+    const session = await getServerSession(authOptions);
+    if (!session?.user) return redirect('/signin');
 
-    // const lesson = await fetchLesson(1);
+    const lesson = await fetchLesson(1);
 
     return (
         <div className="relative my-16 text-left w-full">
-            test
-            {/* <h2 className="text-4xl font-semibold">{lesson.title}</h2>
+            <h2 className="text-4xl font-semibold">{lesson.title}</h2>
             <p className="mt-5">{lesson.description}</p>
 
             <ReactMarkdown className="text-left flex flex-col gap-6 list-disc markdown">
@@ -32,7 +31,7 @@ const page: FC<pageProps> = async () => {
                         Vite Environment <ArrowRight className="" />
                     </span>
                 </Link>
-            </div> */}
+            </div>
         </div>
     );
 };
