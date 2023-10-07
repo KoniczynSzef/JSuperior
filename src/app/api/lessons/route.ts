f8a0d01c61491d6a23364deff8fb7a8e400e31c5
Koniczyn <piotr12konczyk27@gmail.com>
2023-10-07 20:41:17 +0200

    Added another trycatch

--- src/app/api/lessons/route.ts
+++ src/app/api/lessons/route.ts
@@ -2,18 +2,25 @@ import { prisma } from '@/lib/prisma';
 import { Lesson } from '@prisma/client';
 
 export async function GET() {
-    const lessons = await prisma.lesson.findMany();
+    try {
+        const lessons = await prisma.lesson.findMany();
 
-    return new Response(JSON.stringify(lessons));
+        return new Response(JSON.stringify(lessons));
+    } catch (error) {
+        console.error('Error while getting lessons');
+    }
 }
 
 export async function POST(req: Request) {
-    const body: Lesson = await req.json();
-    console.log(body.content);
+    try {
+        const body: Lesson = await req.json();
 
-    const newLesson = await prisma.lesson.create({
-        data: body,
-    });
+        const newLesson = await prisma.lesson.create({
+            data: body,
+        });
 
-    return new Response(JSON.stringify(newLesson));
+        return new Response(JSON.stringify(newLesson));
+    } catch (error) {
+        console.error('Error while creating new lesson');
+    }
 }
