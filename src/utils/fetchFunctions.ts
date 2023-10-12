1da47450f7638ed5390059d3333abe858f14cf4f
Koniczyn <piotr12konczyk27@gmail.com>
2023-10-12 20:54:05 +0200

    Changed response to throwing an error

--- src/utils/fetchFunctions.ts
+++ src/utils/fetchFunctions.ts
@@ -14,7 +14,7 @@ export const fetchLesson = async (id: number) => {
             process.env.NODE_ENV === 'production' &&
             res.headers.get('Content-Type') !== 'application/json'
         ) {
-            return null;
+            throw new Error('Invalid response from server');
         }
 
         const data: Lesson | null = await res.json();
