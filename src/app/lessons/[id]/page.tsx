7d9002fe02aa8ffaaa7b663288eb2d4b18ba9c63
Koniczyn <piotr12konczyk27@gmail.com>
2023-10-21 13:35:57 +0200

    Commented page.tsx in lessons and lessons/id

--- src/app/lessons/[id]/page.tsx
+++ src/app/lessons/[id]/page.tsx
@@ -1,15 +1,16 @@
 import { authOptions } from '@/utils/authOptions';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
-import React, { FC } from 'react';
-import Markdown from './Markdown';
-import QuizWrapper from './QuizWrapper';
-import Link from 'next/link';
-import { ArrowLeft, ArrowRight } from 'lucide-react';
-import { fetchLesson, fetchQuiz } from '@/utils/fetchFunctions';
-import BookMark from '@/components/bookmark/BookMark';
-import { Lesson } from '@prisma/client';
-import { Metadata } from 'next';
+import { FC } from 'react';
+// import React from 'react';
+// import Markdown from './Markdown';
+// import QuizWrapper from './QuizWrapper';
+// import Link from 'next/link';
+// import { ArrowLeft, ArrowRight } from 'lucide-react';
+// import { fetchLesson, fetchQuiz } from '@/utils/fetchFunctions';
+// import BookMark from '@/components/bookmark/BookMark';
+// import { Lesson } from '@prisma/client';
+// import { Metadata } from 'next';
 
 interface pageProps {
     params: {
@@ -17,73 +18,75 @@ interface pageProps {
     };
 }
 
-export async function generateMetadata({
-    params,
-}: pageProps): Promise<Metadata> {
-    const lesson: Lesson | null = await fetchLesson(parseInt(params.id));
-    return {
-        title: lesson?.title,
-    };
-}
+// export async function generateMetadata({
+//     params,
+// }: pageProps): Promise<Metadata> {
+//     const lesson: Lesson | null = await fetchLesson(parseInt(params.id));
+//     return {
+//         title: lesson?.title,
+//     };
+// }
 
 const page: FC<pageProps> = async ({ params = { id: '1' } }) => {
     const session = await getServerSession(authOptions);
     if (!session?.user) return redirect('/signin');
 
-    const lesson: Lesson | null = await fetchLesson(parseInt(params.id));
-    const prevLesson: Lesson | null = await fetchLesson(
-        parseInt(params.id) - 1
-    );
-    const nextLesson: Lesson | null = await fetchLesson(
-        parseInt(params.id) + 1
-    );
+    console.log(params);
 
-    const quiz = await fetchQuiz(parseInt(params.id));
+    // const lesson: Lesson | null = await fetchLesson(parseInt(params.id));
+    // const prevLesson: Lesson | null = await fetchLesson(
+    //     parseInt(params.id) - 1
+    // );
+    // const nextLesson: Lesson | null = await fetchLesson(
+    //     parseInt(params.id) + 1
+    // );
 
-    return lesson ? (
-        <div className="my-16 text-left w-full flex items-center justify-between">
-            <section>
-                <h2 className="text-4xl font-semibold">{lesson.title}</h2>
-                <p className="mt-5">{lesson.description}</p>
+    // const quiz = await fetchQuiz(parseInt(params.id));
 
-                <Markdown content={lesson.content} />
+    // return lesson ? (
+    //     <div className="my-16 text-left w-full flex items-center justify-between">
+    //         <section>
+    //             <h2 className="text-4xl font-semibold">{lesson.title}</h2>
+    //             <p className="mt-5">{lesson.description}</p>
 
-                {quiz && <QuizWrapper quiz={quiz} />}
+    //             <Markdown content={lesson.content} />
 
-                <div className="flex mt-8 justify-between">
-                    {prevLesson && prevLesson.title.length >= 1 && (
-                        <Link
-                            href={`/lessons/${
-                                prevLesson.id === 1 ? '' : prevLesson.id
-                            }`}
-                            className="group"
-                        >
-                            <span className="flex items-center gap-3 text-sec group-hover:text-accent-foreground transition">
-                                <ArrowLeft />
-                                {prevLesson.title}
-                            </span>
-                        </Link>
-                    )}
+    //             {quiz && <QuizWrapper quiz={quiz} />}
 
-                    {nextLesson && nextLesson.title.length >= 1 && (
-                        <Link
-                            href={`/lessons/${nextLesson.id}`}
-                            className="group ml-auto"
-                        >
-                            <span className="flex items-center gap-3 text-sec group-hover:text-accent-foreground transition">
-                                {nextLesson.title}
-                                <ArrowRight />
-                            </span>
-                        </Link>
-                    )}
-                </div>
-            </section>
+    //             <div className="flex mt-8 justify-between">
+    //                 {prevLesson && prevLesson.title.length >= 1 && (
+    //                     <Link
+    //                         href={`/lessons/${
+    //                             prevLesson.id === 1 ? '' : prevLesson.id
+    //                         }`}
+    //                         className="group"
+    //                     >
+    //                         <span className="flex items-center gap-3 text-sec group-hover:text-accent-foreground transition">
+    //                             <ArrowLeft />
+    //                             {prevLesson.title}
+    //                         </span>
+    //                     </Link>
+    //                 )}
 
-            <BookMark session={session} currPageId={parseInt(params.id)} />
-        </div>
-    ) : (
-        <h2>Did not find any lesson</h2>
-    );
+    //                 {nextLesson && nextLesson.title.length >= 1 && (
+    //                     <Link
+    //                         href={`/lessons/${nextLesson.id}`}
+    //                         className="group ml-auto"
+    //                     >
+    //                         <span className="flex items-center gap-3 text-sec group-hover:text-accent-foreground transition">
+    //                             {nextLesson.title}
+    //                             <ArrowRight />
+    //                         </span>
+    //                     </Link>
+    //                 )}
+    //             </div>
+    //         </section>
+
+    //         <BookMark session={session} currPageId={parseInt(params.id)} />
+    //     </div>
+    // ) : (
+    //     <h2>Did not find any lesson</h2>
+    // );
 };
 
 export default page;
--- src/app/lessons/page.tsx
+++ src/app/lessons/page.tsx
@@ -1,14 +1,14 @@
 import { authOptions } from '@/utils/authOptions';
 import { getServerSession } from 'next-auth';
-import Link from 'next/link';
+// import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import React, { FC } from 'react';
-import { ArrowRight } from 'lucide-react';
+// import { ArrowRight } from 'lucide-react';
 
-import ReactMarkdown from 'react-markdown';
-import { fetchLesson } from '@/utils/fetchFunctions';
-import BookMark from '@/components/bookmark/BookMark';
-import { Lesson } from '@prisma/client';
+// import ReactMarkdown from 'react-markdown';
+// import { fetchLesson } from '@/utils/fetchFunctions';
+// import BookMark from '@/components/bookmark/BookMark';
+// import { Lesson } from '@prisma/client';
 import { Metadata } from 'next';
 
 interface pageProps {}
@@ -21,33 +21,35 @@ const page: FC<pageProps> = async () => {
     const session = await getServerSession(authOptions);
     if (!session?.user) return redirect('/signin');
 
-    const lesson: Lesson | null = await fetchLesson(1);
-    console.log(lesson);
+    // const lesson: Lesson | null = await fetchLesson(1);
+    // console.log(lesson);
 
-    return lesson ? (
-        <div className="my-16 text-left w-full flex items-center justify-between">
-            <section>
-                <h2 className="text-4xl font-semibold">{lesson.title}</h2>
-                <p className="mt-5">{lesson.description}</p>
+    // return lesson ? (
+    //     <div className="my-16 text-left w-full flex items-center justify-between">
+    //         <section>
+    //             <h2 className="text-4xl font-semibold">{lesson.title}</h2>
+    //             <p className="mt-5">{lesson.description}</p>
 
-                <ReactMarkdown className="text-left flex flex-col gap-6 list-disc markdown">
-                    {lesson.content}
-                </ReactMarkdown>
+    //             <ReactMarkdown className="text-left flex flex-col gap-6 list-disc markdown">
+    //                 {lesson.content}
+    //             </ReactMarkdown>
 
-                <div className="flex mt-8">
-                    <Link href={`/lessons/2`} className="group ml-auto">
-                        <span className="flex items-center gap-3 text-sec group-hover:text-accent-foreground transition">
-                            Vite Environment <ArrowRight className="" />
-                        </span>
-                    </Link>
-                </div>
-            </section>
+    //             <div className="flex mt-8">
+    //                 <Link href={`/lessons/2`} className="group ml-auto">
+    //                     <span className="flex items-center gap-3 text-sec group-hover:text-accent-foreground transition">
+    //                         Vite Environment <ArrowRight className="" />
+    //                     </span>
+    //                 </Link>
+    //             </div>
+    //         </section>
 
-            <BookMark session={session} currPageId={1} />
-        </div>
-    ) : (
-        <h2>Did not find any lesson</h2>
-    );
+    //         <BookMark session={session} currPageId={1} />
+    //     </div>
+    // ) : (
+    //     <h2>Did not find any lesson</h2>
+    // );
+
+    return <p>Hello World!</p>;
 };
 
 export default page;
