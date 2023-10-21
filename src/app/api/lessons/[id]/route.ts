1c821e1c90d6ff9b2e985d728fbf8bcdc3fa3957
Koniczyn <piotr12konczyk27@gmail.com>
2023-10-21 13:55:13 +0200

    Commented api routes

--- src/app/api/lessons/[id]/route.ts
+++ src/app/api/lessons/[id]/route.ts
@@ -1,18 +1,18 @@
-import { prisma } from '@/lib/prisma';
+// import { prisma } from '@/lib/prisma';
 
-export async function GET(
-    req: Request,
-    { params }: { params: { id: string } }
-) {
-    try {
-        const lesson = await prisma.lesson.findUnique({
-            where: {
-                id: parseInt(params.id),
-            },
-        });
+// export async function GET(
+//     req: Request,
+//     { params }: { params: { id: string } }
+// ) {
+//     try {
+//         const lesson = await prisma.lesson.findUnique({
+//             where: {
+//                 id: parseInt(params.id),
+//             },
+//         });
 
-        return new Response(JSON.stringify(lesson));
-    } catch (error) {
-        throw new Error('Something went wrong while finding a unique lesson');
-    }
-}
+//         return new Response(JSON.stringify(lesson));
+//     } catch (error) {
+//         throw new Error('Something went wrong while finding a unique lesson');
+//     }
+// }
--- src/app/api/lessons/route.ts
+++ src/app/api/lessons/route.ts
@@ -1,26 +1,26 @@
-import { prisma } from '@/lib/prisma';
-import { Lesson } from '@prisma/client';
+// import { prisma } from '@/lib/prisma';
+// import { Lesson } from '@prisma/client';
 
-export async function GET() {
-    try {
-        const lessons = await prisma.lesson.findMany();
+// export async function GET() {
+//     try {
+//         const lessons = await prisma.lesson.findMany();
 
-        return new Response(JSON.stringify(lessons));
-    } catch (error) {
-        throw new Error('Failed to get lessons');
-    }
-}
+//         return new Response(JSON.stringify(lessons));
+//     } catch (error) {
+//         throw new Error('Failed to get lessons');
+//     }
+// }
 
-export async function POST(req: Request) {
-    try {
-        const body: Lesson = await req.json();
+// export async function POST(req: Request) {
+//     try {
+//         const body: Lesson = await req.json();
 
-        const newLesson = await prisma.lesson.create({
-            data: body,
-        });
+//         const newLesson = await prisma.lesson.create({
+//             data: body,
+//         });
 
-        return new Response(JSON.stringify(newLesson));
-    } catch (error) {
-        throw new Error('Error while creating new lesson: ');
-    }
-}
+//         return new Response(JSON.stringify(newLesson));
+//     } catch (error) {
+//         throw new Error('Error while creating new lesson: ');
+//     }
+// }
