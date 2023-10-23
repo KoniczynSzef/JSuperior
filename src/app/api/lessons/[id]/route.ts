00ef6b6cffa2d43909eee629c67ea342b4f4927b
Koniczyn <piotr12konczyk27@gmail.com>
2023-10-23 19:32:07 +0200

    Changed fetchFunctions v3

--- src/app/api/lessons/[id]/route.ts
+++ src/app/api/lessons/[id]/route.ts
@@ -1,39 +1,45 @@
-import { prisma } from '@/lib/prisma';
+// import { prisma } from '@/lib/prisma';
 
-export async function GET(
-    req: Request,
-    { params }: { params: { id: string } }
-) {
-    try {
-        const lesson = await prisma.lesson.findUnique({
-            where: {
-                id: parseInt(params.id),
-            },
-        });
+// export async function GET(
+//     req: Request,
+//     { params }: { params: { id: string } }
+// ) {
+// try {
+//     const lesson = await prisma.lesson.findUnique({
+//         where: {
+//             id: parseInt(params.id),
+//         },
+//     });
 
-        const lessonStr = JSON.stringify(lesson);
+//     const lessonStr = JSON.stringify(lesson);
 
-        // Check if the first character of the JSON string is 'T' and if we are in production environment
-        if (lessonStr.charAt(0) === 'T') {
-            // If so, throw an error to prevent the Response object from being created
-            throw new Error('Failed to get lessons');
-        }
+//     // Check if the first character of the JSON string is 'T' and if we are in production environment
+//     if (lessonStr.charAt(0) === 'T') {
+//         // If so, throw an error to prevent the Response object from being created
+//         throw new Error('Failed to get lessons');
+//     }
 
-        // Create a new Response object with the JSON string and headers
-        const response = new Response(lessonStr, {
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Methods':
-                    'GET, POST, PUT, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers':
-                    'X-Requested-With, Content-Type, Authorization',
-            },
-        });
+//     // Create a new Response object with the JSON string and headers
+//     const response = new Response(lessonStr, {
+//         headers: {
+//             'Access-Control-Allow-Origin': '*',
+//             'Content-Type': 'application/json',
+//             'Access-Control-Allow-Methods':
+//                 'GET, POST, PUT, DELETE, OPTIONS',
+//             'Access-Control-Allow-Headers':
+//                 'X-Requested-With, Content-Type, Authorization',
+//         },
+//     });
 
-        // Return the Response object
-        return response;
-    } catch (error) {
-        throw new Error('Something went wrong while finding a unique lesson');
-    }
+//     // Return the Response object
+//     return response;
+// } catch (error) {
+//     throw new Error('Something went wrong while finding a unique lesson');
+// }
+
+// return new Response('Hello world from lesson/id route');
+// }
+
+export async function GET() {
+    return new Response('Hello world from lesson/id route');
 }
--- src/app/api/lessons/route.ts
+++ src/app/api/lessons/route.ts
@@ -1,62 +1,63 @@
-import { prisma } from '@/lib/prisma';
-import { Lesson } from '@prisma/client';
-import { NextResponse } from 'next/server';
+// import { prisma } from '@/lib/prisma';
+// import { Lesson } from '@prisma/client';
+// import { NextResponse } from 'next/server';
 
 export async function GET() {
-    try {
-        const lessons = await prisma.lesson.findMany();
+    return new Response('Hello world');
+    // try {
+    //     const lessons = await prisma.lesson.findMany();
 
-        const lessonStr = JSON.stringify(lessons);
+    //     const lessonStr = JSON.stringify(lessons);
 
-        // Check if the first character of the JSON string is 'T' and if we are in production environment
-        if (lessonStr.charAt(0) === 'T') {
-            // If so, throw an error to prevent the Response object from being created
-            throw new Error('Failed to get lessons');
-        }
+    //     // Check if the first character of the JSON string is 'T' and if we are in production environment
+    //     if (lessonStr.charAt(0) === 'T') {
+    //         // If so, throw an error to prevent the Response object from being created
+    //         throw new Error('Failed to get lessons');
+    //     }
 
-        // Create a new Response object with the JSON string and headers
-        const response = new Response(lessonStr, {
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Methods':
-                    'GET, POST, PUT, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers':
-                    'X-Requested-With, Content-Type, Authorization',
-            },
-        });
+    //     // Create a new Response object with the JSON string and headers
+    //     const response = new Response(lessonStr, {
+    //         headers: {
+    //             'Access-Control-Allow-Origin': '*',
+    //             'Content-Type': 'application/json',
+    //             'Access-Control-Allow-Methods':
+    //                 'GET, POST, PUT, DELETE, OPTIONS',
+    //             'Access-Control-Allow-Headers':
+    //                 'X-Requested-With, Content-Type, Authorization',
+    //         },
+    //     });
 
-        // Return the Response object
-        return response;
-    } catch (error) {
-        // If an error occurs, throw a new error with a message
-        throw new Error('Failed to get lessons');
-    }
+    //     // Return the Response object
+    //     return response;
+    // } catch (error) {
+    //     // If an error occurs, throw a new error with a message
+    //     throw new Error('Failed to get lessons');
+    // }
 }
 
-export async function POST(req: Request) {
-    try {
-        const body: Lesson = await req.json();
+// export async function POST(req: Request) {
+//     try {
+//         const body: Lesson = await req.json();
 
-        const newLesson = await prisma.lesson.create({
-            data: body,
-        });
+//         const newLesson = await prisma.lesson.create({
+//             data: body,
+//         });
 
-        const res = NextResponse.json(newLesson);
+//         const res = NextResponse.json(newLesson);
 
-        res.headers.set('Access-Control-Allow-Origin', '*');
-        res.headers.set('Content-Type', 'text/plain');
-        res.headers.set(
-            'Access-Control-Allow-Methods',
-            'GET, POST, PUT, DELETE, OPTIONS'
-        );
-        res.headers.set(
-            'Access-Control-Allow-Headers',
-            'X-Requested-With, Content-Type, Authorization'
-        );
+//         res.headers.set('Access-Control-Allow-Origin', '*');
+//         res.headers.set('Content-Type', 'text/plain');
+//         res.headers.set(
+//             'Access-Control-Allow-Methods',
+//             'GET, POST, PUT, DELETE, OPTIONS'
+//         );
+//         res.headers.set(
+//             'Access-Control-Allow-Headers',
+//             'X-Requested-With, Content-Type, Authorization'
+//         );
 
-        return res;
-    } catch (error) {
-        throw new Error('Error while creating new lesson: ');
-    }
-}
+//         return res;
+//     } catch (error) {
+//         throw new Error('Error while creating new lesson: ');
+//     }
+// }
