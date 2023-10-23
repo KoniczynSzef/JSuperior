2a6af4541eff8c483c00dd929df1c94e5821f446
Koniczyn <piotr12konczyk27@gmail.com>
2023-10-23 18:59:46 +0200

    Another fight started

--- src/app/api/lessons/route.ts
+++ src/app/api/lessons/route.ts
@@ -1,26 +1,26 @@
 import { prisma } from '@/lib/prisma';
 import { Lesson } from '@prisma/client';
-
 import { NextResponse } from 'next/server';
 
 export async function GET() {
     try {
-        const lessons = await prisma.lesson.findMany();
+        // const lessons = await prisma.lesson.findMany();
 
-        const response = NextResponse.json(lessons);
+        // const response = NextResponse.json(lessons);
 
-        response.headers.set('Access-Control-Allow-Origin', '*');
-        response.headers.set('Content-Type', 'text/plain');
-        response.headers.set(
-            'Access-Control-Allow-Methods',
-            'GET, POST, PUT, DELETE, OPTIONS'
-        );
-        response.headers.set(
-            'Access-Control-Allow-Headers',
-            'X-Requested-With, Content-Type, Authorization'
-        );
+        // response.headers.set('Access-Control-Allow-Origin', '*');
+        // response.headers.set('Content-Type', 'text/plain');
+        // response.headers.set(
+        //     'Access-Control-Allow-Methods',
+        //     'GET, POST, PUT, DELETE, OPTIONS'
+        // );
+        // response.headers.set(
+        //     'Access-Control-Allow-Headers',
+        //     'X-Requested-With, Content-Type, Authorization'
+        // );
 
-        return response;
+        // return response;
+        return new Response(JSON.stringify(await prisma.lesson.findMany()));
     } catch (error) {
         throw new Error('Failed to get lessons');
     }
@@ -34,20 +34,20 @@ export async function POST(req: Request) {
             data: body,
         });
 
-        const response = NextResponse.json(newLesson);
+        const res = NextResponse.json(newLesson);
 
-        response.headers.set('Access-Control-Allow-Origin', '*');
-        response.headers.set('Content-Type', 'text/plain');
-        response.headers.set(
+        res.headers.set('Access-Control-Allow-Origin', '*');
+        res.headers.set('Content-Type', 'text/plain');
+        res.headers.set(
             'Access-Control-Allow-Methods',
             'GET, POST, PUT, DELETE, OPTIONS'
         );
-        response.headers.set(
+        res.headers.set(
             'Access-Control-Allow-Headers',
             'X-Requested-With, Content-Type, Authorization'
         );
 
-        return response;
+        return res;
     } catch (error) {
         throw new Error('Error while creating new lesson: ');
     }
