339c79c3743b4b47df92e1d33dd4dbcdae65c230
Koniczyn <piotr12konczyk27@gmail.com>
2023-10-23 20:41:47 +0200

    Added meta attr to layout v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
             <html lang="en" suppressHydrationWarning>
                 <meta
                     httpEquiv="Content-Security-Policy"
-                    content="connect-src 'none'"
+                    content="connect-src 'self' 'https://jsuperior.vercel.app'"
                 />
                 <body
                     className={`${inter.className} bg-gradient-b from-gray-900 via-[#300171] to-slate-900 text-foreground`}
