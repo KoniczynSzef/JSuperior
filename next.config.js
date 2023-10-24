c73fc4a954ba5534c767faddcd190a25d73747ed
Koniczyn <piotr12konczyk27@gmail.com>
2023-10-24 16:42:09 +0200

    Changed next.config

--- next.config.js
+++ next.config.js
@@ -7,23 +7,6 @@ module.exports = {
                 protocol: 'https',
                 hostname: 'avatars.githubusercontent.com',
             },
-            {
-                protocol: 'https',
-                hostname: '',
-            },
         ],
-    },
-    async headers() {
-        return [
-            {
-                source: '/api/lessons',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: "connect-src 'self' https://jsuperior.vercel.app",
-                    },
-                ],
-            },
-        ];
     },
 };
